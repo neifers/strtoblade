c3dfdf0bd89b936a84f148ad02e3bf0f4d6e60bd
Edwin Ricaurte <edwin@neifers.com>
2022-03-12 18:09:24 -0500

    Starting Project

---
+++ src/StrToBlade.php
@@ -0,0 +1,22 @@
+<?php namespace Neifers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Engines\CompilerEngine;
+
+class StrToBlade extends ServiceProvider {
+
+    public function boot(){
+
+    }
+
+    public function register()
+    {
+        $this->app->singleton(StrToBlade::class);
+
+        $this->app->alias(StrToBlade::class, 'dbview');
+    }
+
+	public function render(String $string) {
+		return 'Hi '.$string;
+	}
+}
