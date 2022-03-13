5bf08aefa83f1d564e33950fc61b8d70f57a3dbd
Neifers <101466894+neifers@users.noreply.github.com>
2022-03-12 20:26:08 -0500

    Update StrToBlade.php

--- src/StrToBlade.php
+++ src/StrToBlade.php
@@ -19,27 +19,27 @@ class StrToBlade extends ServiceProvider {
     }
 
 	public function render(String $string, String $blade_variable_name, Object $data) {
-        $filename = uniqid('email_');
-        $path = storage_path("/app/db-blade-compiler/views/");
-        $filepath = "$path$filename.blade.php";
+		$filename = uniqid('email_');
+		$path = storage_path("/app/db-blade-compiler/views/");
+		$filepath = "$path$filename.blade.php";
 
-        $old_template_files_list = $file_names = preg_grep('~^.*\.php$~', \scandir($path));
-        foreach($old_template_files_list as $old_template_file){
-            $diff_in_seconds = (strtotime("now") - strtotime(date('m/d/Y H:i:s',filemtime($path.$old_template_file))));
-            if($diff_in_seconds >= 300){
-                File::delete($path.$old_template_file);
-            }
-        }
+		$old_template_files_list = $file_names = preg_grep('~^.*\.php$~', \scandir($path));
+		foreach($old_template_files_list as $old_template_file){
+			$diff_in_seconds = (strtotime("now") - strtotime(date('m/d/Y H:i:s',filemtime($path.$old_template_file))));
+			if($diff_in_seconds >= 300){
+			File::delete($path.$old_template_file);
+			}
+		}
 
-        if(!file_exists(storage_path("/app/db-blade-compiler/views"))) {
-            mkdir(storage_path("/app/db-blade-compiler/views"), 0777, true);
-        }
+		if(!file_exists(storage_path("/app/db-blade-compiler/views"))) {
+			mkdir(storage_path("/app/db-blade-compiler/views"), 0777, true);
+		}
 
-        file_put_contents($filepath, trim($string));
-        $rendered = (View($filename, [$blade_variable_name => $data]))->render();
+		file_put_contents($filepath, trim($string));
+		$rendered = (View($filename, [$blade_variable_name => $data]))->render();
 
-        File::delete($filepath);
+		File::delete($filepath);
 
-        return $rendered;
+		return $rendered;
 	}
 }
